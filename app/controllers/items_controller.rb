389a582a7f4bae8bed02edf9a56740d2a09c2cac
Doug Kintop <dkintop@hotmail.com>
2019-10-27 12:42:06 -0400

    created item post and show routes

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,3 +1,24 @@
 class ItemsController <ApplicationController
 
+
+get '/items' do 
+    @items = Item.all  #eventually you will make this so only the items from the store that the user belongs to will display.
+    erb :'/items/index'
+end
+
+
+get '/posts/new' do
+
+    erb:'posts/new'
+end
+
+
+get '/items/:id' do 
+    @item = Item.find_by_id(params['id'])
+    erb :'items/show'
+end
+
+
+
+
 end 
