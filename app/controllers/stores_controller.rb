d95d59e07ff1d2a362c4500a20c161cfe1645a27
Doug Kintop <dkintop@hotmail.com>
2019-10-27 14:42:40 -0400

    created stores new and show actions

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,3 +1,25 @@
 class StoresController < ApplicationController
+    
+    get '/stores' do 
+        @stores = Store.all #change this to display only stores that the current user belongs to.
+        erb :"stores/index"
+    end
+
+    get '/stores/new' do 
+        erb :"stores/new"
+    end
+
+    post '/stores' do 
+        @store = Store.create(:name =>params[:name], :location => params[:location]) 
+        redirect to "/stores/#{@store.id}"
+    end 
+
+    get '/stores/:id' do 
+        @store = Store.find_by_id(params[:id])
+        erb :"stores/show"
+    end 
+
+
+
 
 end
