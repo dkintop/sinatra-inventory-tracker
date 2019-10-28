1fe2a0ba3c28fe74a1eabb4edada9f043a4558aa
Doug Kintop <dkintop@hotmail.com>
2019-10-28 09:25:50 -0400

    fixed user sign up mechanics to log user in directly after creating user account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,22 +9,26 @@ post '/Users/registrations/Users/registrations' do
     @user= User.new 
     @user.username = params[:username] 
     @user.password = params[:password]
-    @user.store = Store.find_by_id(params[:store_id])
+    @user.store = Store.find_by_id(params[:store_id]) #this form gets info posted from user signup.erb
     @user.name = params[:name]
     @user.save
     session[:user_id]= @user.id
     if session[:user_id] == @user.id
     redirect to "/Users/#{@user.id}"
+    else
+        puts "error"
     end
 end
 
 get '/Users/:id' do
-    binding.pry
     @user = User.find_by_id(params[:id])
-    @store = @user.store
-   
-    erb :'Users/users/show'
     
+    @store = @user.store
+   if session[:user_id] == @user.id
+    erb :'Users/users/show'
+   else
+    redirect to '/'
+   end
 end
 
 
