246b7a4afd7068460147d585195224a0c09a7fd0
Doug Kintop <dkintop@hotmail.com>
2019-10-29 09:47:44 -0400

    fixed bug that would drop user_id from session upon redirect after log in.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
-    set :sessions_secret, "thisisnogood"
+    set :session_secret, "thisisnogood"
   end
 
 
